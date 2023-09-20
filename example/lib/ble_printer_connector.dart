c6bc20713a88c165cd2f6c27153971cbc044c01e
nathanatpheedloop <nathan@pheedloop.com>
2023-09-20 14:27:00 -0400

    get write characteristic when connected

--- example/lib/ble_printer_connector.dart
+++ example/lib/ble_printer_connector.dart
@@ -4,6 +4,13 @@ import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:flutter_zebra_sdk_example/reactive_state.dart';
 
 class BlePrinterConnector extends ReactiveState<ConnectionStateUpdate> {
+  static final Uuid _serviceUuid =
+      Uuid.parse('38EB4A80-C570-11E3-9507-0002A5D5C51B');
+  static final Uuid _write2CharacteristicsUuid =
+      Uuid.parse("38EB4A82-C570-11E3-9507-0002A5D5C51B");
+  static final Uuid _readCharacteristicsUuid =
+      Uuid.parse("38EB4A81-C570-11E3-9507-0002A5D5C51B");
+
   BlePrinterConnector({
     required FlutterReactiveBle ble,
     required Function(String message) logMessage,
@@ -17,14 +24,19 @@ class BlePrinterConnector extends ReactiveState<ConnectionStateUpdate> {
   final Map<String, DeviceConnectionState> _printerConnectionStates = {};
 
   @override
-  Stream<ConnectionStateUpdate> get state => _deviceConnectionController.stream;
+  Stream<PrinterConnectionStatusUpdate> get state =>
+      _deviceConnectionController.stream;
 
-  final _deviceConnectionController = StreamController<ConnectionStateUpdate>();
+  final _deviceConnectionController =
+      StreamController<PrinterConnectionStatusUpdate>();
 
   Future<void> connect(String deviceId) async {
     if (_printerConnectionSubscriptions.containsKey(deviceId) &&
         _printerConnectionStates.containsKey(deviceId) &&
-        _printerConnectionStates[deviceId] == DeviceConnectionState.connected) {
+        (_printerConnectionStates[deviceId] ==
+                DeviceConnectionState.connected ||
+            _printerConnectionStates[deviceId] ==
+                DeviceConnectionState.connecting)) {
       throw Exception('Already connected to $deviceId');
     }
 
@@ -33,9 +45,42 @@ class BlePrinterConnector extends ReactiveState<ConnectionStateUpdate> {
     _printerConnectionSubscriptions[deviceId] =
         _ble.connectToDevice(id: deviceId).listen(
       (update) async {
+        if (update.connectionState == DeviceConnectionState.connected) {
+          await _ble.discoverAllServices(deviceId);
+          final allServices = await _ble.getDiscoveredServices(deviceId);
+
+          if (!(allServices.any((service) =>
+              service.id == _serviceUuid &&
+              service.characteristics.any((characteristic) =>
+                  characteristic.id == _write2CharacteristicsUuid) &&
+              service.characteristics.any((characteristic) =>
+                  characteristic.id == _readCharacteristicsUuid)))) {
+            // if not a valid zebra printer, then disconnect
+            await disconnect(deviceId);
+            return;
+          }
+
+          final requestedMtu =
+              await _ble.requestMtu(deviceId: deviceId, mtu: 512);
+          final writeCharacteristic = allServices
+              .firstWhere((service) => service.id == _serviceUuid)
+              .characteristics
+              .firstWhere((characteristic) =>
+                  characteristic.id == _write2CharacteristicsUuid);
+
+          final printerUpdate =
+              PrinterConnectionStatusUpdate.withConnectionStateUpdate(
+            update,
+            mtu: requestedMtu,
+            writeCharacteristic: writeCharacteristic,
+          );
+
+          _deviceConnectionController.add(printerUpdate);
+        }
+
+        _printerConnectionStates[deviceId] = update.connectionState;
         _logMessage(
             'ConnectionState for printer $deviceId : ${update.connectionState}');
-        _deviceConnectionController.add(update);
       },
       onError: (Object e) =>
           _logMessage('Connecting to printer $deviceId resulted in error $e'),
@@ -58,34 +103,45 @@ class BlePrinterConnector extends ReactiveState<ConnectionStateUpdate> {
       _logMessage("Error disconnecting from a printer: $e");
     } finally {
       _deviceConnectionController.add(
-        ConnectionStateUpdate(
+        PrinterConnectionStatusUpdate(
           deviceId: deviceId,
           connectionState: DeviceConnectionState.disconnected,
-          failure: null,
         ),
       );
     }
   }
 
-//   Future<void> sendWrite() async {
-// try {
-//       _logMessage('disconnecting to device: $deviceId');
-//       _ble.writeCharacteristicWithoutResponse(, value: value)
-//     } on Exception catch (e, _) {
-//       _logMessage("Error disconnecting from a device: $e");
-//     } finally {
-//       // Since [_connection] subscription is terminated, the "disconnected" state cannot be received and propagated
-//       _deviceConnectionController.add(
-//         ConnectionStateUpdate(
-//           deviceId: deviceId,
-//           connectionState: DeviceConnectionState.disconnected,
-//           failure: null,
-//         ),
-//       );
-//     }
-//   }
-
   Future<void> dispose() async {
     await _deviceConnectionController.close();
+  }
+}
+
+class PrinterConnectionStatusUpdate extends ConnectionStateUpdate {
+  final int? mtu;
+  final Characteristic? writeCharacteristic;
+
+  const PrinterConnectionStatusUpdate({
+    required String deviceId,
+    required DeviceConnectionState connectionState,
+    GenericFailure<ConnectionError>? failure,
+    this.mtu,
+    this.writeCharacteristic,
+  }) : super(
+            deviceId: deviceId,
+            connectionState: connectionState,
+            failure: failure);
+
+  factory PrinterConnectionStatusUpdate.withConnectionStateUpdate(
+    ConnectionStateUpdate update, {
+    int? mtu,
+    Characteristic? writeCharacteristic,
+  }) {
+    return PrinterConnectionStatusUpdate(
+      deviceId: update.deviceId,
+      connectionState: update.connectionState,
+      failure: update.failure,
+      mtu: mtu,
+      writeCharacteristic: writeCharacteristic,
+    );
   }
 }
